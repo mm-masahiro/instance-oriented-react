3171f92b8a2ea1ab336a1ef1f70877e79d8abb5f
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-08 23:54:06 +0900

    set

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Film from './Film';
 
 interface Props {};
 
 const App: React.FC<Props> = (props: Props) => {
   return (
     <div className='App'>
-      <h1>インスタンス指向 React</h1>      
+      <h1>インスタンス指向 React</h1>
+      <Film title='ラストエンペラー' />
+      <Film title='幸福の黄色いハンカチ' />
+      <Film title='秒速5センチメートル' />
+      <Film title='パンドラの匣' />
     </div>
   )
 };
--- src/Film.tsx
+++ src/Film.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 interface Props {
   title: string;
-  watchDate: string;
-  point: number;
-  note: string;
+  watchDate?: string;
+  point?: number;
+  note?: string;
 }
 
 const Film: React.FC<Props> = (props: Props) => {
@@ -12,7 +12,9 @@ const Film: React.FC<Props> = (props: Props) => {
     <div>
       <h2>{props.title}</h2>
       <p>鑑賞日: {props.watchDate}</p>
-      <p>評: {'★'.repeat(props.point)}</p>
+      {props.point && (
+        <p>評: {'★'.repeat(props.point)}</p>
+      )}
       <p>感想: {props.note}</p>
     </div>
   )
