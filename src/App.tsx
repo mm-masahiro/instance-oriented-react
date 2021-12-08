38966268d0441545e1c87a5ae51f3fb1ec4750a2
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-08 23:39:23 +0900

    fix App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface Props {};
 
-export default App;
+const App: React.FC<Props> = (props: Props) => {
+  return (
+    <div className='App'>
+      <h1>インスタンス指向 React</h1>      
+    </div>
+  )
+};
+
+export default App
